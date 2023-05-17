036b277ee60859764fe8c1fdfe046867dec96e75
bradfordwagner <wagner.bradford@gmail.com>
2023-05-16 23:55:08 -0400

    create a test cluster

--- test/providers.tf
+++ test/providers.tf
@@ -4,6 +4,16 @@ terraform {
       source  = "justenwalker/kind"
       version = ">=0.17.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">=2.9.0"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = pathexpand("~/.kube/${var.cluster_name}")
   }
 }
 
--- test/tf_module_test.go
+++ test/tf_module_test.go
@@ -33,7 +33,7 @@ func TestTerraformBasicExample(t *testing.T) {
 
 	// website::tag::4::Clean up resources with "terraform destroy". Using "defer" runs the command at the end of the test, whether the test succeeds or fails.
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
-	defer terraform.Destroy(t, terraformOptions)
+	// defer terraform.Destroy(t, terraformOptions)
 
 	// website::tag::2::Run "terraform init" and "terraform apply".
 	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
